cc50bd0e50790dbb8c73ff2c63998bd91bd0e0c5
mj94 <cmoonjun11@gmail.com>
2025-11-29 12:59:18 +0900

    style: 코드 포맷팅 및 문서 정리

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,8 +1,7 @@
-import { defineRouting } from 'next-intl/routing';
+import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ['ko', 'en', 'ja'],
-  defaultLocale: 'ko',
-  localePrefix: 'always',
+  locales: ["ko", "en", "ja"],
+  defaultLocale: "ko",
+  localePrefix: "always",
 });
-
